a2c7a934e085de04656fe0e396ccdd8f9d455310
Ava <avamsteimle@gmail.com>
2022-07-02 21:58:43 -0700

    initial set up/layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Header from './components/Header.js';
+import Main from './components/Main.js';
+import Footer from './components/Footer.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="page-wrap">
+    <header className="page-header">
+      <Header />
+    </header>
+    <main className="page-main">
+      <Main />
+    </main>
+    <footer className="page-footer">
+      <Footer />
+    </footer>
+  </div>
   );
 }
 
