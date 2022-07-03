7d5af370b5582772d699d8a3c8f02acebdbf0eac
Ava <avamsteimle@gmail.com>
2022-07-02 22:09:42 -0700

    header message

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 const Header = () => {
     return(
         <div>
-
+            <h1>welcome to my site</h1>
+            <h2>you wont regret coming here</h2>
+            <h3>but if you do thats ok</h3>
+            <h4>you can always leave</h4>
         </div>
     );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,6 @@
 const Main = () => {
     return(
         <div>
-
         </div>
     );
 }
