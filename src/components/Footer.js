ecc0cb603dba46861a1cd1f976bcf8c7f2a67642
Ava <avamsteimle@gmail.com>
2022-07-02 23:04:10 -0700

    finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,18 @@
+import {SocialMediaIconsReact} from 'social-media-icons-react';
+
 const Footer = () => {
     return(
         <div>
-
+<SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" 
+borderWidth="5" 
+borderStyle="solid" 
+icon="instagram" 
+iconColor="rgba(255,255,255,1)" 
+backgroundColor="#F8CF2C" 
+iconSize="5" 
+roundness="50%" 
+url="https://instagram.com/i_am_france_" 
+size="100" />
         </div>
     );
 }
